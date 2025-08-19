3de36d07685ff6b31aa8f8f5d155a1c028f295c1
Artem Basalaev <basa62.1996@gmail.com>
2025-08-20 00:15:39 +0200

    Install cert-manager

--- terraform/base/main.tf
+++ terraform/base/main.tf
@@ -11,3 +11,17 @@ resource "helm_release" "vault" {
     yamlencode(yamldecode(file("${path.module}/config/helm-vault-values.yaml"))),
   ]
 }
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "oci://quay.io/jetstack/charts"
+  chart      = "cert-manager"
+  version    = "v1.18.2"
+
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  values = [
+    yamlencode(yamldecode(file("${path.module}/config/helm-cert-manager-values.yaml"))),
+  ]
+}
